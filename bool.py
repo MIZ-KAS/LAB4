b82de2b7db9d3c286d990d2edf9df16b36a83894
MIS-KAS <kshakirat0@gmail.com>
2018-07-25 15:40:25 +0100

    ADDING BOOL FILE

---
+++ bool.py
@@ -0,0 +1,26 @@
+radius = 5
+
+if ( radius < 0 ):
+    print ("TRUE")
+else:
+    print ("FALSE")
+
+if ( radius > 0 ):
+    print ("TRUE")
+else:
+    print ("FALSE")
+
+if ( radius >= 0 ):
+    print ("TRUE")
+else:
+    print ("FALSE")
+
+if ( radius == 0 ):
+    print ("TRUE")
+else:
+    print ("FALSE")
+
+if ( radius != 0 ):
+    print ("TRUE")
+else:
+    print ("FALSE")
